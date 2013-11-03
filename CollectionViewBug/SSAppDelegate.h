ff162a8f8abfc7dad428dfdd1060f786cd6bdb8d
Alexander Selling <alexander@sellingsolutions.se>
2013-11-03 15:31:07 +0100

    Initial Commit

---
+++ CollectionViewBug/SSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SSAppDelegate.h
+//  CollectionViewBug
+//
+//  Created by Alexander Selling on 03/11/13.
+//  Copyright (c) 2013 SellingSolutions Scandinavia AB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionViewBug/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CollectionViewBug
+//
+//  Created by Alexander Selling on 03/11/13.
+//  Copyright (c) 2013 SellingSolutions Scandinavia AB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SSAppDelegate class]));
+    }
+}
